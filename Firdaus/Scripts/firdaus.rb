f53b7370967de4a219f74c36354716f9033719a0
ashes999 <ashes999@yahoo.com>
2015-11-02 20:28:59 -0500

    Monday night dev session.

--- Firdaus/Scripts/firdaus.rb
+++ Firdaus/Scripts/firdaus.rb
@@ -1,4 +1,5 @@
 require 'scripts/mods/full_screen_plus_plus'
 require 'scripts/mods/lemonys_sound_emitting_events'
+require 'scripts/mods/ace_item_menu'
 
 Graphics.fullscreen_mode
---
+++ going-to-the-moon/Scripts/main.rb
@@ -0,0 +1,2 @@
+require 'scripts/mods/full_screen_plus'
+Graphics.fullscreen_mode
