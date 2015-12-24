62f3a50fef0d0ab84355711e4038e2914c608a46
ashes999 <ashes999@yahoo.com>
2015-12-23 23:48:35 -0500

    One month of changes. Don't ask.

--- Firdaus/Scripts/firdaus.rb
+++ Firdaus/Scripts/firdaus.rb
@@ -1,5 +1,6 @@
 require 'scripts/mods/full_screen_plus_plus'
 require 'scripts/mods/lemonys_sound_emitting_events'
 require 'scripts/mods/ace_item_menu'
+require 'scripts/mods/Yanfly_Party_System'
 
 Graphics.fullscreen_mode
